04e1db5e2ba6355765ca228fa6dc42c211c5ee98
Siarhei Luk <jlamep7@gmail.com>
2021-12-21 20:40:16 +0300

    update some changes

--- SampleMusic/Chat Detail Screen/View/ChatDetailViewController.swift
+++ SampleMusic/Chat Detail Screen/View/ChatDetailViewController.swift
@@ -17,8 +17,6 @@ class ChatDetailViewController: UIViewController, UITableViewDataSource, UITable
         super.init(nibName: nil, bundle: nil)
     }
     
-    
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
